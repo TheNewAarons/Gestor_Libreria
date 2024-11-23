91f98a133bcd334d0ce22919110f4566d276f818
Aaron Soto <Sotoaaronsukoaisv@gmail.com>
2024-11-22 21:19:19 -0300

    migracion a base de datos MySQL

--- GestorLibreria/settings.py
+++ GestorLibreria/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'GestorLibreria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'MySQL',
+        "USER": "root",
+        "PASSWORD": "abc123",
+        "HOST": "/var/run/mysql",
+        "PORT": "5432",
     }
 }
 
