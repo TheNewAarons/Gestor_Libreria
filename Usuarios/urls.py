340bbd49fbe07dafd5a186ac7bda2fda05f8c63d
bastian <bastianjofre4000@gmail.com>
2024-11-28 22:53:30 -0300

    implementacion para la seguridad de accesos al sistema

--- Usuarios/urls.py
+++ Usuarios/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from Usuarios.views import UsuarioListView, UsuarioCreateView, UsuarioDeleteView, UsuarioUpdateView, UsuarioDetailView
+from . import views
+
 urlpatterns = [
     path('listUsuarios/', UsuarioListView.as_view(), name = 'userList'),
     path('CreateUsuarios/', UsuarioCreateView.as_view(), name = 'userCreate'),
     path('DeleteUsuarios/<int:pk>', UsuarioDeleteView.as_view(), name = 'userDelete'),
     path('UpdateUsuarios/<int:pk>', UsuarioUpdateView.as_view(), name = 'userUpdate'),
-    path('DetailUsuarios/<int:pk>', UsuarioDetailView.as_view(), name = 'userDetail')
+    path('DetailUsuarios/<int:pk>', UsuarioDetailView.as_view(), name = 'userDetail'),
+    path('no-autorizado/', views.no_autorizado, name='no_autorizado') #view para el aviso sobre el ingreso no autorizado al sistema
+    
 ]
--- Usuarios/views.py
+++ Usuarios/views.py
@@ -1,29 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from Usuarios.models import Users
 from Usuarios.forms import UsersForm
 from django.urls import reverse_lazy
+from django.http import HttpResponse
 # Create your views here.
 
-class UsuarioListView(ListView):
+#Clase para poder asegurar las vistas para los diferentes roles que tenemos, esta clase funciona en base al rol que otorgemos y asi evitar que usuarios sin permisos ingresen a otras funcionalidades
+class RolRequeridoMixin(UserPassesTestMixin):
+    rol_requerido = 'Jefe de Bodega' 
+
+    #Funcion para obtener el rol del usuario y restringir la vista, igual en las demas ya que va basado en nuestros requerimientos
+    def test_func(self): #Va a evaluar que el usuario corresponda al rol que usemos para las restricciones
+        return self.request.user.rol == self.rol_requerido
+
+    #Funcion para redirigir al usuario que ingrese a la vista sin estar registrado o si inicio sesion con otro rol, hacia una pagina con un mensaje
+    def handle_no_permission(self):
+        return redirect('no_autorizado') 
+
+#Funcion para mostrar en una pagina un mensaje sobre el acesso al sistema mediante un error 403
+def no_autorizado(request):
+    return render(request, 'Usuarios/ventanaError.html', status=403)
+
+class UsuarioListView(LoginRequiredMixin,RolRequeridoMixin,ListView):
     model = Users
     template_name = 'Usuarios/userList.html'
     context_object_name = 'usuarios'
+    rol_requerido = 'Jefe de Bodega'
+    login_url = 'registration/login.html'
+    redirect_field_name = 'Usuarios/userList.html'
     
-class UsuarioCreateView(CreateView):
+    
+class UsuarioCreateView(LoginRequiredMixin,RolRequeridoMixin,CreateView):
     model = Users
     form_class = UsersForm
     template_name = 'Usuarios/userCreate.html'
     success_url = reverse_lazy('userList')
+    rol_requerido = 'Jefe de Bodega'
+    login_url = 'registration/login.html'
+    redirect_field_name = 'Usuarios/userCreate.html'
 
-class UsuarioUpdateView(UpdateView):
+
+class UsuarioUpdateView(LoginRequiredMixin,RolRequeridoMixin,UpdateView):
     model = Users
     form_class = UsersForm
     template_name = 'Usuarios/userUpdate.html'
     success_url = reverse_lazy('userList')
+    rol_requerido = 'Jefe de Bodega'
+    login_url = 'registration/login.html'
+    redirect_field_name = 'Usuarios/userUpdate.html'
+    
 
-class UsuarioDeleteView(DeleteView):
+class UsuarioDeleteView(LoginRequiredMixin,RolRequeridoMixin,DeleteView):
     model = Users
     template_name = 'Usuarios/userDelete.html'
     success_url = reverse_lazy('userList')
@@ -32,8 +61,14 @@ class UsuarioDeleteView(DeleteView):
         user = self.get_object()  # obtiene el objeto del usuario con la id (pk) de la URL
         context['username'] = user.username  # agrega el username al contexto para usarlo en la plantilla
         return context
+    rol_requerido = 'Jefe de Bodega'
+    login_url = 'registration/login.html'
+    redirect_field_name = 'Usuarios/userDelete.html'
     
-class UsuarioDetailView(DetailView):
+class UsuarioDetailView(LoginRequiredMixin,RolRequeridoMixin,DetailView):
     model = Users
     template_name = 'Usuarios/userDetail.html'
     context_object_name = 'usuario'
+    rol_requerido = 'Jefe de Bodega'
+    login_url = 'registration/login.html'
+    redirect_field_name = 'Usuarios/userDetail.html'
